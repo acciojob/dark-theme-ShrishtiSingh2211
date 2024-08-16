0e3dd3e0f74dfe0f1a002002172b3869b1f8d31a
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-08-16 20:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
-
+function swapTheme() {
+var appDiv = document.getElementById("app");
+var swapButton=document.getElementById("swap");
+	if(appDiv.classList.contanins("day")){
+		appDiv.classList.remove("day")
+		appDiv.classList.add("night")
+	}
+	else{
+		appDiv.classList.remove("night")
+		appDiv.classList.add("day")
+	}
+	if(swapButton.classList.contanins("button_day"))
+	{
+		swapButton.classList.remove("button_day")
+		swapButton.classList.add("button_night")
+	}
+	else{
+		swapButton.classList.remove("button_night")
+		swapButton.classList.add("button_day")
+	}
+}
